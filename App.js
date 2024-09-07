931aab85c66710c0842666657950dae6f0a5607f
ADK <adk.kda10@gmail.com>
2024-09-07 18:08:26 +0000

    Secind commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Movie
-        
+      <Text>Movi
+
       </Text>
       <StatusBar style="auto" />
     </View>
